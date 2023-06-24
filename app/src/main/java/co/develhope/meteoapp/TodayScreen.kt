fc76bea777868e7796c140c6dfa01aa7fdd2aed7
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-06-24 16:04:34 +0200

    fixed dimen file and style file

--- app/src/main/java/co/develhope/meteoapp/TodayScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/TodayScreen.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.develhope.meteoapp.databinding.TodayScreenBinding
+
 
 class TodayScreen : Fragment() {
+    private lateinit var binding: TodayScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,29 @@ class TodayScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.today_screen, container, false)
+        binding = TodayScreenBinding.inflate(inflater, container, false)
+        val todayInfoList : ArrayList<WeatherConditions> = arrayListOf(
+            WeatherConditions("11:00",R.drawable.sun_image,"24°","33%","45°","5/10","23%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("12:00",R.drawable.sun_image,"26°","13%","55°","5/10","13%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("13:00",R.drawable.sun_image,"27°","13%","50°","5/10","23%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("14:00",R.drawable.sun_image,"25°","16%","50°","5/10","33%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("15:00",R.drawable.sun_image,"26°","17%","47°","5/10","13%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("16:00",R.drawable.sun_image,"24°","15%","45°","5/10","12%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("17:00",R.drawable.sun_image,"23°","13%","43°","5/10","25%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("18:00",R.drawable.sun_image,"28°","33%","42°","5/10","hHo","SSE 7km/h","30%","0cm"),
+            WeatherConditions("19:00",R.drawable.sun_image,"30°","22%","43°","5/10","12%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("20:00",R.drawable.sun_image,"34°","13%","39°","5/10","15%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("21:00",R.drawable.sun_image,"33°","17%","28°","5/10","16%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("22:00",R.drawable.sun_image,"37°","19%","68°","5/10","17%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("23:00",R.drawable.sun_image,"36°","10%","47°","5/10","18%","SSE 7km/h","30%","0cm"),
+            WeatherConditions("00:00",R.drawable.sun_image,"35°","18%","45°","5/10","20%","SSE 7km/h","30%","0cm")
+
+        )
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = TodayInfoAdapter(todayInfoList)
+
+
+        return binding.root
     }
 }
