b912b83a13e61dfdb10a2422e4b1f4acbc2b8eca
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-06-27 17:12:24 +0200

    added dimen, styles, strings and fixed home card view and layout

--- app/src/main/java/co/develhope/meteoapp/ui/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/adapter/HomeScreenAdapter.kt
@@ -1,14 +1,17 @@
 package co.develhope.meteoapp.ui.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.R
 import co.develhope.meteoapp.network.data.WeatherData
 import co.develhope.meteoapp.databinding.WeatherInfoHomeBinding
 
-class HomeScreenAdapter(private val items : WeatherData) : RecyclerView.Adapter<HomeScreenAdapter.ViewHolder>() {
+class HomeScreenAdapter(private val dataset: List<HomeScreenItem>) : RecyclerView.Adapter<HomeScreenAdapter.ViewHolder>() {
 
-    inner class ViewHolder(private val binding: WeatherInfoHomeBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ViewHolder(private val binding: WeatherInfoHomeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun onBind(item: WeatherData, position: Int) {
             val temperatureMax = item.daily.temperature2mMax[position]
             val temperatureMin = item.daily.temperature2mMin[position]
@@ -17,12 +20,29 @@ class HomeScreenAdapter(private val items : WeatherData) : RecyclerView.Adapter<
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(WeatherInfoHomeBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return ViewHolder(
+            WeatherInfoHomeBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(items[position])
+    }
+
+    class CardViewHolder(private val binding: WeatherInfoHomeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(cardItem: HomeScreenItem.CardItem) {
+            binding. =
+            )
+
+
+        }
     }
 }
