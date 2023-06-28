923493f0eda26fca2b4301b9ceab84050fdd0de4
VasiFran <127098109+VasiFran@users.noreply.github.com>
2023-06-28 12:10:20 +0200

    edited the recycler view data class and info displayed

--- app/src/main/java/co/develhope/meteoapp/WeatherInfoItem.kt
+++ app/src/main/java/co/develhope/meteoapp/WeatherInfoItem.kt
@@ -1,3 +1,8 @@
 package co.develhope.meteoapp
 
-data class WeatherInfoItem(val text: String)
+data class WeatherInfoItem(val weekday: String,
+                           val date: String,
+                           val min_temp: String,
+                           val max_temp: String,
+                           val wind_speed: String,
+                           val rain: String)
