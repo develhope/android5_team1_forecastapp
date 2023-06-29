964d597bfcf92234a8badf72f932d34cf79412bd
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-06-29 17:44:21 +0200

    added network provider with retrofit istance

--- app/src/main/java/co/develhope/meteoapp/network/NetworkProvider.kt
+++ app/src/main/java/co/develhope/meteoapp/network/NetworkProvider.kt
@@ -1,4 +1,28 @@
 package co.develhope.meteoapp.network
 
+import co.develhope.meteoapp.network.service.WeatherAPI
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class NetworkProvider {
+    private val BASE_URL = "https://api.open-meteo.com/"
+    private val interceptor = HttpLoggingInterceptor()
+
+    init {
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val okhttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okhttpClient)
+        .build()
+
+    val api = retrofit.create(WeatherAPI::class.java)
+
 }
