01472ae102fc191e79a94b38e75c59840ca2be35
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-07-29 14:15:55 +0200

    added if statement for night mode

--- app/src/main/java/co/develhope/meteoapp/network/local/WeatherType.kt
+++ app/src/main/java/co/develhope/meteoapp/network/local/WeatherType.kt
@@ -3,21 +3,29 @@ package co.develhope.meteoapp.network.local
 import co.develhope.meteoapp.R
 
 enum class WeatherType {
-    SUNNY, CLOUDY, RAINY;
+    SUNNY, CLOUDY, RAINY, FOGGY, THUNDERSTORM, HEAVYRAIN, NIGHT;
 
     fun imageWeatherType(): Int {
         return when (this) {
             SUNNY -> R.drawable.sun
             RAINY -> R.drawable.rain
             CLOUDY -> R.drawable.sun_cloud
+            THUNDERSTORM -> R.drawable.rain
+            FOGGY -> R.drawable.sun_cloud
+            HEAVYRAIN -> R.drawable.rain
+            NIGHT -> R.drawable.moon
             else -> R.drawable.moon
         }
     }
 }
 fun Int?.getWeatherType(): WeatherType {
     return when(this) {
-        0 -> WeatherType.SUNNY
+        0  -> WeatherType.SUNNY
         1, 2, 3 -> WeatherType.CLOUDY
-        else -> WeatherType.RAINY
+        in 45..48 -> WeatherType.FOGGY
+        in 51..77 -> WeatherType.RAINY
+        80, 81, 82 -> WeatherType.HEAVYRAIN
+        in 95..99 -> WeatherType.HEAVYRAIN
+        else -> WeatherType.CLOUDY
     }
 }
