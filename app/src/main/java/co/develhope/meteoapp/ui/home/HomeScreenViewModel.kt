497aa13b5a41f9bd32fc19a1593bd7e90e97fff4
VasiFran <127098109+VasiFran@users.noreply.github.com>
2023-07-31 19:22:00 +0200

    Modified View Model, Adapter and view (INCOMPLETE)

--- app/src/main/java/co/develhope/meteoapp/ui/home/HomeScreenViewModel.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/home/HomeScreenViewModel.kt
@@ -1,5 +1,6 @@
 package co.develhope.meteoapp.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,21 +8,19 @@ import co.develhope.meteoapp.MyApplicationMeteo
 import co.develhope.meteoapp.data.local.Place
 import co.develhope.meteoapp.data.local.WeatherSummary
 import co.develhope.meteoapp.network.NetworkProvider
-import co.develhope.meteoapp.network.remote.WeeklySummary
 import kotlinx.coroutines.launch
 import org.threeten.bp.OffsetDateTime
 
 sealed class WeeklyResult() {
-    data class Result(val list: List<WeatherSummary>, val place: Place, val date: OffsetDateTime) : WeeklyResult()
+    data class Result(val list: List<WeatherSummary>, val place: Place, val date: OffsetDateTime) :
+        WeeklyResult()
 }
 
 class HomeScreenViewModel : ViewModel() {
 
-    private val networkProvider = NetworkProvider()
-    private val weatherAPI = networkProvider.weatherAPI
-
     private val _weeklyWeatherData = MutableLiveData<WeeklyResult>()
-    val weeklyWeatherData : MutableLiveData<WeeklyResult> = _weeklyWeatherData
+    val weeklyWeatherData : LiveData<WeeklyResult> = _weeklyWeatherData
+
 
     fun getWeeklyWeather(){
         viewModelScope.launch {
@@ -29,7 +28,10 @@ class HomeScreenViewModel : ViewModel() {
                 val place = MyApplicationMeteo.preferences?.getPrefPlace()
                 val date = OffsetDateTime.now()
                 if (place != null && date != null) {
-                val weeklyForecast = NetworkProvider().getWeeklySummary(place.latitude,place.longitude)
+                    val weeklyForecast = NetworkProvider().getWeeklySummary(
+                        place.latitude,
+                        place.longitude
+                    )
                 _weeklyWeatherData.value = WeeklyResult.Result(weeklyForecast, place, date)
                 }
             }catch (e: Exception){
