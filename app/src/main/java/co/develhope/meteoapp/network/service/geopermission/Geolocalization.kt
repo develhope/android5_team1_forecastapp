3a56a3b017df6bb15d337dd3b9339f7f06ebadee
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-08-02 16:04:24 +0200

    need to add getcty and get country

--- app/src/main/java/co/develhope/meteoapp/network/service/geopermission/Geolocalization.kt
+++ app/src/main/java/co/develhope/meteoapp/network/service/geopermission/Geolocalization.kt
@@ -3,17 +3,23 @@ package co.develhope.meteoapp.network.service.geopermission
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import co.develhope.meteoapp.data.local.Place
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
 
 object Geolocalization {
 
-    var fusedLocationProvideClient:
-
     private fun getCurrentPosition(context: Context) {
+        var fusedLocationProvideClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(context)
+        
         if(checkPermission(context)) {
             if(isLocationEnabled(context)) {
                 if(ActivityCompat.checkSelfPermission(
@@ -26,9 +32,24 @@ object Geolocalization {
                 ) {
                     return
                 }
+                fusedLocationProvideClient.lastLocation.addOnCompleteListener() { 
+                    val location: Location? = it.result
+                    if(location != null) {
+                        Toast.makeText(context, "Localizzazione avvenuta con successo", Toast.LENGTH_LONG).show()
+                        val place = Place(
+                            latitude = location.latitude,
+                            longitude = location.longitude,
+                            city = getCity()
+                        )
+                    }
+                }
 
             }
         }
+    }
+
+    private fun getCity(): String {
+
     }
 
     private fun checkPermission(context: Context): Boolean {
